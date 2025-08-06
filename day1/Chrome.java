190387e589b2e32ae336cdd3eee3457222362fda
Dillipkumar0502 <dilvdk1984@gmail.com>
2025-08-06 22:07:11 +0530

    Add files via upload
    Day 1 Assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         float version = 91.0f;
+         String developer = "Google";
+         boolean isBeta = false;
+         int releaseYear = 2008;
+         char shortcutKey = 'C';
+        		 System.out.println("The Version of Chrome is: "+version+"\n"+"The Developer of Chrome is: "+developer+"\n"+"The Boolean Value is: "+isBeta+"\n"+"The Release Year of Chrome is: "+releaseYear+"\n"+"The Short Cut Key to Open Chrome is: "+shortcutKey);
+         
+	}
+
+}
